a877f957444052fd8ed6233f81338ad95b6aff27
Matias Varas <matias.varas62@gmail.com>
2023-11-13 16:47:37 -0300

    actualizacion

--- src/pages/admin/Contactos.jsx
+++ src/pages/admin/Contactos.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import {RiAddFill, RiDeleteBin2Fill,RiEdit2Line} from 'react-icons/ri'
+
+
 
 const Contactos = () => {
   return (
     <div>
-      Contactos
+      <div className='flex items-center justify-between gap-y-4'>
+        <div>
+          <h1 className='font-bold text-gray-100 text-xl'>ADMINISTRA TUS CLIENTES</h1>
+        </div>
+        <div className='flex flex-row gap-2'>
+          <button className='bg-secondary-100/50 flex items-center gap-2 py-2 px-4 rounded-lg hover'><RiAddFill/> Agregar </button>
+          <button className='bg-secondary-100/50 flex items-center gap-2 py-2 px-4 rounded-lg hover'> <RiEdit2Line/>    Editar </button>
+          <button className='bg-secondary-100/50 flex items-center gap-2 py-2 px-4 rounded-lg hover'> <RiDeleteBin2Fill/>Eliminar </button>
+        </div>
+      </div>
+      <table className='w-full bg-white shadow mt-10 table-auto'>
+        <thead className='bg-blue-800 text-white'>
+          <tr>
+            <th className='p-2'>Nombre</th>
+            <th className='p-2'>Apellido</th>
+            <th className='p-2'>Email</th>
+            <th className='p-2'>Ciudad</th>
+            <th className='p-2'>Telefono</th>
+            <th className='p-2'>Mensaje</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   )
 }
--- src/pages/admin/Maquinas.jsx
+++ src/pages/admin/Maquinas.jsx
@@ -13,7 +13,17 @@ const Maquinas = () => {
           <button className='bg-secondary-100/50 flex items-center gap-2 py-2 px-4 rounded-lg hover'> <RiEdit2Line/>    Editar </button>
           <button className='bg-secondary-100/50 flex items-center gap-2 py-2 px-4 rounded-lg hover'> <RiDeleteBin2Fill/>Eliminar </button>
         </div>
-
+      </div>
+      <div className='flex flex-row items-center justify-between mt-[10vh]'>
+        <div>
+            <img src="/src/assets/maquina_snack_bebestible.png" alt="" />
+        </div>
+        <div>
+          <img src="/src/assets/maquina_bebestible.png" alt="" />
+        </div>
+        <div>
+          <img src="/src/assets/maquina_cafe2.png" alt="" />
+        </div>
       </div>
     </div>
   )
